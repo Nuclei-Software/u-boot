57aa9034c1f3967ea9541f401f2575a80f130a5b
Huaqi Fang <578567190@qq.com>
2023-10-25 20:59:30 +0800

    allow reallocate bigger than 4G
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- arch/riscv/cpu/generic/dram.c
+++ arch/riscv/cpu/generic/dram.c
@@ -22,17 +22,17 @@ int dram_init_banksize(void)
 
 ulong board_get_usable_ram_top(ulong total_size)
 {
-#ifdef CONFIG_64BIT
-	/*
-	 * Ensure that we run from first 4GB so that all
-	 * addresses used by U-Boot are 32bit addresses.
-	 *
-	 * This in-turn ensures that 32bit DMA capable
-	 * devices work fine because DMA mapping APIs will
-	 * provide 32bit DMA addresses only.
-	 */
-	if (gd->ram_top > SZ_4G)
-		return SZ_4G;
-#endif
+//#ifdef CONFIG_64BIT
+//	/*
+//	 * Ensure that we run from first 4GB so that all
+//	 * addresses used by U-Boot are 32bit addresses.
+//	 *
+//	 * This in-turn ensures that 32bit DMA capable
+//	 * devices work fine because DMA mapping APIs will
+//	 * provide 32bit DMA addresses only.
+//	 */
+//	if (gd->ram_top > SZ_4G)
+//		return SZ_4G;
+//#endif
 	return gd->ram_top;
 }
