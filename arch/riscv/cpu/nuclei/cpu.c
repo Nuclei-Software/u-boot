fbfff0b074206809840d60c3636db007adb309a0
guibing <guibing@nucleisys.com>
2024-08-12 09:13:54 +0800

    driver: support cpu cache in spl stage.
    Signed-off-by: guibing <guibing@nucleisys.com>

--- arch/riscv/cpu/nuclei/cpu.c
+++ arch/riscv/cpu/nuclei/cpu.c
@@ -2,6 +2,7 @@
 #include <common.h>
 #include <irq_func.h>
 #include <asm/cache.h>
+#include <asm/arch-nuclei/csr.h>
 
 /*
  * cleanup_before_linux() is called just before we call linux
@@ -16,4 +17,19 @@ int cleanup_before_linux(void)
 	cache_flush();
 
 	return 0;
+}
+
+void icache_enable(void)
+{
+#if CONFIG_IS_ENABLED(RISCV_MMODE)
+	csr_set(CSR_MCACHE_CTL, CSR_MCACHE_ICACHE_EN
+			| CSR_MCACHE_ICACHE_PF_EN | CSR_MCACHE_ICACHE_CANCLE_EN);
+#endif
+}
+
+void dcache_enable(void)
+{
+#if CONFIG_IS_ENABLED(RISCV_MMODE)
+	csr_set(CSR_MCACHE_CTL, CSR_MCACHE_DCACHE_EN);
+#endif
 }
---
+++ arch/riscv/include/asm/arch-nuclei/csr.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2024 Nucleisys Technology Corporation
+ */
+
+#ifndef _ASM_NUCLEI_CSR_H
+#define _ASM_NUCLEI_CSR_H
+
+#include <asm/asm.h>
+#include <linux/const.h>
+
+#define CSR_MCACHE_CTL 0x7ca
+#define CSR_MMISC_CTL 0x7d0
+
+
+#define CSR_MCACHE_ICACHE_EN			BIT(0)
+#define CSR_MCACHE_ICACHE_PF_EN			BIT(6)
+#define CSR_MCACHE_ICACHE_CANCLE_EN		BIT(7)
+#define CSR_MCACHE_DCACHE_EN			BIT(16)
+
+
+#endif /* _ASM_NUCLEI_CSR_H */
