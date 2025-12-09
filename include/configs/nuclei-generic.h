47d24cf7cda1e6bca6dacb698049579be36bc73d
guibing <guibing@nucleisys.com>
2025-12-09 15:39:20 +0800

    include/configs: add boot cmd from sd or flash.
    Signed-off-by: guibing <guibing@nucleisys.com>

--- include/configs/nuclei-generic.h
+++ include/configs/nuclei-generic.h
@@ -22,10 +22,14 @@
 #define CFG_EXTRA_ENV_SETTINGS \
 	"fdt_high=0xffffffffffffffff\0" \
 	"initrd_high=0xffffffffffffffff\0" \
-	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"  \
-	"fdt_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"  \
-	"scriptaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"  \
-	"ramdisk_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"  \
+	"mmcdev=0x0\0" \
+	"fit_load_addr=" __stringify(CONFIG_KERNEL_FIT_LOAD_ADDR) "\0" \
+	"dtb_load_addr=" __stringify(CONFIG_DTB_LOAD_ADDR) "\0" \
+	"mmcloadfit=fatload mmc ${mmcdev} ${fit_load_addr} kernel_rootfs.itb \0" \
+	"mmcboot_fit= echo Trying load from mmc..; run mmcloadfit; bootm ${fit_load_addr}:kernel ${fit_load_addr}:ramdisk  ${dtb_load_addr} \0" \
+	"kernel_flash_addr=" __stringify(CONFIG_KERNEL_FLASH_ADDR) "\0" \
+	"kernel_flash_size=" __stringify(CONFIG_KERNEL_FLASH_SIZE) "\0" \
+	"flashboot_fit= echo Trying load from xipflash..; cp.b ${kernel_flash_addr} ${fit_load_addr} ${kernel_flash_size}; bootm ${fit_load_addr}:kernel ${fit_load_addr}:ramdisk ${dtb_load_addr} \0" \
 	BOOTENV
 
 #define CONFIG_PREBOOT \
